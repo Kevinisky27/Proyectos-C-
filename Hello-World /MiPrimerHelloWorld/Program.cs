a964b18852cfa13050c4eb9240fee18f360ad629
Kevinisky27 <keare27@gmail.com>
2021-02-19 16:57:15 -0600

    Primer Commit de Hola Mundo

---
+++ Hello-World /MiPrimerHelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiPrimerHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+        }
+    }
+}
