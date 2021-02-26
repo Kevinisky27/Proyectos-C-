0c352234154851e086790eaa11f707ed39647cdb
Kevinisky27 <keare27@gmail.com>
2021-02-26 14:36:42 -0600

    Se modifico la opcion 6 y se agrego una, puesto a que la opcion 7 despliega todas las operaciones

--- Menú_Operaciones_Básicas/Op_Basico/Program.cs
+++ Menú_Operaciones_Básicas/Op_Basico/Program.cs
@@ -8,6 +8,8 @@ namespace Op_Basico
         {
             String NombreUsuario; 
             byte Operacion; 
+            float Numero1; 
+            float Numero2; 
 
             // Solicitud a la peticion de nombre del Usuario
             Console.WriteLine("Favor escribe tu nombre completo: ");
@@ -29,36 +31,47 @@ namespace Op_Basico
                 case 1: 
                     Console.WriteLine("\n ----- SUMA -----");
                     break;
-                
+
+
                 // Segundo Caso para validar la RESTA
                 case 2: 
                     Console.WriteLine("\n ----- RESTA -----"); 
                     break;
-                
+
+
                 // Tercer caso para validar la MULTIPLICACION 
                 case 3: 
                     Console.WriteLine("\n ----- MULTIPLICACIÓN -----"); 
                     break;
+
 
                 // Cuarto caso para validar la DIVISION     
                 case 4: 
                     Console.WriteLine("\n ----- DIVISIÓN -----"); 
                     break;
 
+
                 // Quinto caso para validar la POTENCIACION 
                 case 5: 
                     Console.WriteLine("\n ----- POTENCIACIÓN -----"); 
                     break;
 
-                // Sexta opcion de salir
+
+                // Sexta opcion, la cual despliega todas las opciones
                 case 6: 
+                    Console.WriteLine("\n ----- TODAS LAS OPCIONES -----");
+                    break;
+
+
+                // Septima Opcion, la cual sale del programa
+                case 7: 
                     Console.WriteLine("\n ----- SALIR -----");
                     break;
+
 
                 // Error al ingresar un valor no existente dentro de nuestro programa
                 default: 
                     Console.WriteLine("\n ----- ERROR DE OPERACIÓN -----"); 
-
                     // Mensaje de error personalisado con la variable de NombreUsuario, la cual fue solicitada al inicio de la ejecucion del programa
                     Console.WriteLine("     Estimado usiario: " + NombreUsuario + "\n La peticion solicitada no se encuentra dentro de nuestras operaciones. \n \n Le solicitamos volver a correr el software.");
                     break;
