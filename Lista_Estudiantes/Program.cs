2a37c68d07c050eaa1c76e6cd30948e5e3cf5025
Kevinisky27 <keare27@gmail.com>
2021-04-07 02:45:52 -0600

    Se agregaron los liniamientos en los cuales se debe de trabajar, con sus respectivos datos de programacion

--- Lista_Estudiantes/Program.cs
+++ Lista_Estudiantes/Program.cs
@@ -6,7 +6,21 @@ namespace Lista_Estudiantes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Crea en C# un programa que capture una lista de estudiantes, cada estudiante debe poseer los siguientes atributos:
+
+            // Nombre,  == String 
+            // edad,  == Int 
+            // carnet,  == String 
+            // solvente,  == Bool
+
+            // Curso(cada curso debe contener un nombre, nota parcial1, nota parcial2, nota final)
+
+            // *Adjuntar un documento con SCREENSHOTS de la ejecución del programa, como también link de GitHub del programa Realizado
+
+            // Fecha de entrega: miércoles 7 de abril del 2021
+            // Puntos posibles: 2
+
+            
         }
     }
 }
